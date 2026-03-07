396d6a15325f66aaeaa72ddb9e49991605c6de5e
SnpCoder <2899673199@qq.com>
2026-03-07 15:18:17 +0800

    feat<ops>: support for add-inplace ops

--- include/ops/cuda/math/add_cuda.cuh
+++ include/ops/cuda/math/add_cuda.cuh
@@ -8,6 +8,10 @@ namespace cuda {
 void launch_add_kernel_float32(const float* a, const float* b, float* out, size_t n);
 
 void launch_add_scalar_kernel_float32(const float* a, float scalar, float* out, size_t n);
+
+void launch_add_inplace_kernel_float32(float* a, const float* b, size_t n);
+
+void launch_add_scalar_inplace_kernel_float32(float* a, float scalar, size_t n);
 }  // namespace cuda
 }  // namespace miniDL
 #endif  // __MINIDL_OPERATOR_ADD_CUDA_H__
--- src/ops/cuda/math/add_cuda.cu
+++ src/ops/cuda/math/add_cuda.cu
@@ -38,6 +38,39 @@ __global__ void add_scalar_vectorized_float4_kernel(const float* __restrict__ A,
     }
 }
 
+__global__ void add_inplace_vectorized_float4_kernel(float* __restrict__ A,
+                                                     const float* __restrict__ B, size_t N) {
+    size_t idx = (blockIdx.x * blockDim.x + threadIdx.x) * 4;
+
+    if (idx + 4 <= N) {
+        float4 a = reinterpret_cast<float4*>(&A[idx])[0];
+        float4 b = reinterpret_cast<const float4*>(&B[idx])[0];
+
+        a.x += b.x;
+        a.y += b.y;
+        a.z += b.z;
+        a.w += b.w;
+        reinterpret_cast<float4*>(&A[idx])[0] = a;
+    } else {
+        for (size_t i = idx; i < N && i < idx + 4; ++i) { A[i] += B[i]; }
+    }
+}
+
+__global__ void add_scalar_inplace_vectorized_float4_kernel(float* __restrict__ A, float scalar,
+                                                            size_t N) {
+    size_t idx = (blockIdx.x * blockDim.x + threadIdx.x) * 4;
+    if (idx + 4 <= N) {
+        float4 a = reinterpret_cast<float4*>(&A[idx])[0];
+        a.x += scalar;
+        a.y += scalar;
+        a.z += scalar;
+        a.w += scalar;
+        reinterpret_cast<float4*>(&A[idx])[0] = a;
+    } else {
+        for (size_t i = idx; i < N && i < idx + 4; ++i) { A[i] += scalar; }
+    }
+}
+
 // for host
 void launch_add_kernel_float32(const float* a, const float* b, float* out, size_t n) {
     if (n == 0) return;
@@ -64,5 +97,32 @@ void launch_add_scalar_kernel_float32(const float* a, float scalar, float* out, 
         MINIDL_THROW_RUNTIME("CUDA add_scalar kernel failed: %s", cudaGetErrorString(err));
     }
 }
+
+void launch_add_inplace_kernel_float32(float* a, const float* b, size_t n) {
+    if (n == 0) return;
+    int threads = 256;
+    int blocks  = (n + (threads * 4) - 1) / (threads * 4);
+
+    add_inplace_vectorized_float4_kernel<<<blocks, threads>>>(a, b, n);
+
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        MINIDL_THROW_RUNTIME("CUDA add_inplace kernel failed: %s", cudaGetErrorString(err));
+    }
+}
+
+void launch_add_scalar_inplace_kernel_float32(float* a, float scalar, size_t n) {
+    if (n == 0) return;
+    int threads = 256;
+    int blocks  = (n + (threads * 4) - 1) / (threads * 4);
+
+    add_scalar_inplace_vectorized_float4_kernel<<<blocks, threads>>>(a, scalar, n);
+
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        MINIDL_THROW_RUNTIME("CUDA add_scalar_inplace kernel failed: %s", cudaGetErrorString(err));
+    }
+}
+
 }  // namespace cuda
 }  // namespace miniDL
